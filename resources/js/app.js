bc24c51366b3aed20a3f6e1dc499c312877205e9
thetrekmen <jonespm1@gmail.com>
2022-07-02 10:38:05 -0400

    added a modal to all the projects

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,41 +1,63 @@
 /* Get In Touch Modal */
+  const modal = document.querySelector(".modal");
+  const openModal = document.querySelector(".open-button");
+  const closeModal = document.querySelector(".close-button");
 
-const modal = document.querySelector("#modal");
-const openModal = document.querySelector(".open-button");
-const closeModal = document.querySelector(".close-button");
+  /* Opens the modal and controls CSS variables for transitions and animations */
+  openModal.addEventListener("click", () => {
+    modal.showModal();
+  });
 
-/* Opens the modal and controls CSS variables for transitions and animations */
-openModal.addEventListener("click", () => {
-  modal.showModal();
-});
-
-/* Default way for closing the modal and controls CSS variables for transitions and animations */
-closeModal.addEventListener("click", () => {
-  modal.close();
-});
-
-
-
-
-/* Another way for closing the modal and controls CSS variables for transitions and animations */
-/* document.addEventListener("keydown", (e) => {
-  if (e.key === "ESCAPE") {
+  /* Default way for closing the modal and controls CSS variables for transitions and animations */
+  closeModal.addEventListener("click", () => {
     modal.close();
-    // if (modal.close()) {
-    //   modal.style.display = "none";
-    //   modal.style.top = "0";
-    // }
-  }
-}); */
+  });
 
 
+/* Copy Email */
+  const sideEmail = document.querySelector(".side-email");
+  const email = "jonespm1.dev@gmail.com";
 
-const sideEmail = document.querySelector(".side-email");
-const email = "jonespm1.dev@gmail.com";
+  sideEmail.addEventListener('click', function(event) {
+    
+    navigator.clipboard.writeText(email);
+  });
 
-sideEmail.addEventListener('click', function(event) {
-  
-	navigator.clipboard.writeText(email);
-});
+
+/* Project Modals */
+
+/* Modal 1 */
+const projectModal1 = document.querySelector("#modal1");
+const openProject1 = document.querySelector("#open-project1");
+
+  openProject1.addEventListener("click", () => {
+    projectModal1.showModal();
+  });
+
+/* Modal 2 */
+const projectModal2 = document.querySelector("#modal2");
+const openProject2 = document.querySelector("#open-project2");
+
+  openProject2.addEventListener("click", () => {
+    projectModal2.showModal();
+  });
+
+/* Modal 3 */
+const projectModal3 = document.querySelector("#modal3");
+const openProject3 = document.querySelector("#open-project3");
+
+  openProject3.addEventListener("click", () => {
+    projectModal3.showModal();
+  });
+
+/* Modal 4 */
+const projectModal4 = document.querySelector("#modal4");
+const openProject4 = document.querySelector("#open-project4");
+
+  openProject4.addEventListener("click", () => {
+    projectModal4.showModal();
+  });
+
+
 
 
