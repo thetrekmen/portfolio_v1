df25934f6ca0e3ec26948278d1345b09b689a959
thetrekmen <jonespm1@gmail.com>
2022-07-02 03:37:26 -0400

    modal animation in good.  working on out still

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,33 @@
+/* Get In Touch Modal */
+
 const modal = document.querySelector("#modal");
 const openModal = document.querySelector(".open-button");
 const closeModal = document.querySelector(".close-button");
 
+/* Opens the modal and controls CSS variables for transitions and animations */
 openModal.addEventListener("click", () => {
   modal.showModal();
 });
 
+/* Default way for closing the modal and controls CSS variables for transitions and animations */
 closeModal.addEventListener("click", () => {
   modal.close();
 });
+
+
+
+
+/* Another way for closing the modal and controls CSS variables for transitions and animations */
+/* document.addEventListener("keydown", (e) => {
+  if (e.key === "ESCAPE") {
+    modal.close();
+    // if (modal.close()) {
+    //   modal.style.display = "none";
+    //   modal.style.top = "0";
+    // }
+  }
+}); */
+
 
 
 const sideEmail = document.querySelector(".side-email");
